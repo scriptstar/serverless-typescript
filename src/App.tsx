a03eacc6b4d2b9c73ee3c27cb7e31b480d54d294
Narendra <scriptstar@hotmail.co.uk>
2022-05-19 17:59:22 +0100

    feat: working Typescript form + serverless function

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Form } from "./components/Form";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header className="App">
+        <h1>Sereverless Typescript + React</h1>
+      </header>
+      <main>
+        <Form />
+      </main>
+    </>
+  );
+}
+export default App;
